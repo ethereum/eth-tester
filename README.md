55e5dfb5ebe5367adf3b74da2431a2d1641cb851
Piper Merriam <pipermerriam@gmail.com>
2017-07-05 16:01:47 -0600

    stub out basic library structure

---
+++ README.md
@@ -0,0 +1,17 @@
+# Ethereum Tester
+
+[![Join the chat at https://gitter.im/pipermerriam/ethereum-tester](https://badges.gitter.im/pipermerriam/ethereum-tester.svg)](https://gitter.im/pipermerriam/ethereum-tester?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
+[![Build Status](https://travis-ci.org/pipermerriam/ethereum-tester.png)](https://travis-ci.org/pipermerriam/ethereum-tester)
+
+
+Tools for testing ethereum based applications.
+
+
+## Installation
+
+```sh
+pip install ethereum-tester
+```
+
+TODO
---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+readme = open(os.path.join(DIR, 'README.md')).read()
+
+
+setup(
+    name='ethereum-tester',
+    version='0.1.0-alpha.1',
+    description="""Tools for testing Ethereum applications.""",
+    long_description=readme,
+    author='Piper Merriam',
+    author_email='pipermerriam@gmail.com',
+    url='https://github.com/pipermerriam/ethereum-tester',
+    include_package_data=True,
+    install_requires=[],
+    py_modules=['eth_tester'],
+    license="MIT",
+    zip_safe=False,
+    keywords='ethereum',
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
