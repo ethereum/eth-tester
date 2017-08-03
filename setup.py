52a395b7795f660dedb5d3fa545d9a90a4b8f819
Piper Merriam <pipermerriam@gmail.com>
2017-08-03 00:11:10 -0600

    0.1.0-alpha.2 Release

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ readme = open(os.path.join(DIR, 'README.md')).read()
 
 setup(
     name='ethereum-tester',
-    version='0.1.0-alpha.1',
+    version='0.1.0-alpha.2',
     description="""Tools for testing Ethereum applications.""",
     long_description=readme,
     author='Piper Merriam',
