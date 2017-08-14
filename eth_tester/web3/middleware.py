a890f070b658d4d80f8de311ee16ca4e331d2a29
Piper Merriam <pipermerriam@gmail.com>
2017-08-14 11:31:26 -0600

    more tests passing

--- eth_tester/web3/middleware.py
+++ eth_tester/web3/middleware.py
@@ -1,3 +1,6 @@
+from cytoolz.functoolz import (
+    complement,
+)
 from eth_utils import (
     is_string,
 )
@@ -12,10 +15,17 @@ from web3.utils.formatters import (
 )
 
 
+def is_named_block(value):
+    return value in {"latest", "earliest", "pending"}
+
+
+is_not_named_block = complement(is_named_block)
+
+
 class EthereumTesterFormatterMiddleware(BaseFormatterMiddleware):
     request_formatters = {
         'eth_getBlockByNumber': apply_formatter_at_index(
-            apply_formatter_if(hex_to_integer, is_string),
+            apply_formatter_if(hex_to_integer, is_not_named_block),
             0,
         ),
     }
