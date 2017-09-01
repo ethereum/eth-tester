efdf8b8642f95a4da330bebe258d12f86aed1ff3
njgheorghita <nickgheorghita@gmail.com>
2017-09-01 10:42:55 -0400

    Get accounts passing

--- eth_tester/backends/pyevm/main.py
+++ eth_tester/backends/pyevm/main.py
@@ -2,14 +2,19 @@ from __future__ import absolute_import
 
 import pkg_resources
 
-from semantic_version import (
-    Spec,
-)
-
 from eth_tester.backends.base import BaseChainBackend
 from eth_tester.backends.pyevm.utils import (
     get_pyevm_version,
     is_pyevm_available,
+)
+from eth_tester.constants import (
+    FORK_HOMESTEAD,
+    FORK_DAO,
+    FORK_ANTI_DOS,
+    FORK_STATE_CLEANUP,
+)
+from eth_tester.exceptions import (
+    UnknownFork,
 )
 
 
@@ -30,10 +35,23 @@ class PyEvmBackend(BaseChainBackend):
         self.evm = tester.state()
 
     #
+    # Fork Rules
+    #
+    def set_fork_block(self, fork_name, fork_block):
+        if fork_name == FORK_HOMESTEAD:
+            self.evm.config['HOMESTEAD_FORK_BLKNUM'] = fork_block
+        elif fork_name == FORK_DAO:
+            self.evm.config['DAO_FORK_BLKNUM'] = fork_block
+        elif fork_name == FORK_ANTI_DOS:
+            self.evm.config['ANTI_DOS_FORK_BLKNUM'] = fork_block
+        elif fork_name == FORK_STATE_CLEANUP:
+            self.evm.config['CLEARING_FORK_BLKNUM'] = fork_block
+        else:
+            raise UnknownFork("Unknown fork name: {0}".format(fork_name))
+
+    #
     # Accounts
     #
     def get_accounts(self):
-        from pyevm.tools import tester
-
-        for account in tester.accounts:
-            yield account
+        from evm.tools import tester
+        return tester.accounts
