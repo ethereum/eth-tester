f3ca1c6e4a842664d55a874e4d796ce485200658
Piper Merriam <pipermerriam@gmail.com>
2017-09-27 12:18:31 -0600

    missing receipts_root field

--- eth_tester/backends/pyevm/serializers.py
+++ eth_tester/backends/pyevm/serializers.py
@@ -43,17 +43,18 @@ def serialize_block(block, full_transaction, is_pending):
         "sha3_uncles": block.header.uncles_hash,
         "logs_bloom": block.header.bloom,
         "transactions_root": block.header.transaction_root,
+        "receipt_root": block.header.receipt_root,
         "state_root": block.header.state_root,
         "miner": block.header.coinbase,
         "difficulty": block.header.difficulty,
-        "total_difficulty": block.header.difficulty,  # TODO
+        "total_difficulty": block.header.difficulty,  # TODO: actual total difficulty
         "size": len(rlp.encode(block)),
         "extra_data": pad32(block.header.extra_data),
         "gas_limit": block.header.gas_limit,
         "gas_used": block.header.gas_used,
         "timestamp": block.header.timestamp,
         "transactions": transactions,
-        "uncles": [],  # TODO
+        "uncles": [uncle.hash for uncle in block.uncles],
     }
 
 
