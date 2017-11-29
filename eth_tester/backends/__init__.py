a53f51795f899d0095d7775a31ab4c61c0c6bca7
Piper Merriam <piper@pipermerriam.com>
2017-11-28 23:14:14 -0700

    Update __init__.py

--- eth_tester/backends/__init__.py
+++ eth_tester/backends/__init__.py
@@ -15,6 +15,10 @@ from .pyethereum.v16 import (
     PyEthereum16Backend,
     is_pyethereum16_available,
 )
+from .pyethereum.v20 import (
+    PyEthereum20Backend,
+    is_pyethereum20_available,
+)
 from .pyevm import (  # noqa: F401
     PyEVMBackend,
     is_pyevm_available,
@@ -32,6 +36,8 @@ def get_chain_backend_class(backend_import_path=None):
             if vi.major != 3 or vi.minor < 5:
                 warnings.warn(UserWarning("Py-EVM does not support python < 3.5"))
             backend_import_path = get_import_path(PyEVMBackend)
+        elif is_pyethereum20_available():
+            backend_import_path = get_import_path(PyEthereum20Backend)
         elif is_pyethereum16_available():
             backend_import_path = get_import_path(PyEthereum16Backend)
         else:
