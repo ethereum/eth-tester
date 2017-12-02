e089bda167c29b339bfdbd8a2bf04dbc87357f82
Piper Merriam <piper@pipermerriam.com>
2017-12-02 08:34:03 -0700

    fix accessing chaindb on chain class.

--- eth_tester/backends/pyevm/main.py
+++ eth_tester/backends/pyevm/main.py
@@ -223,7 +223,7 @@ class PyEVMBackend(object):
     def revert_to_snapshot(self, snapshot):
         block = self.chain.get_block_by_hash(snapshot)
         header = self.chain.create_header_from_parent(block.header)
-        self.chain = type(self.chain)(chaindb=self.chaindb, header=header)
+        self.chain = type(self.chain)(chaindb=self.chain.chaindb, header=header)
 
     def reset_to_genesis(self):
         self.account_keys, self.chain = setup_tester_chain()
