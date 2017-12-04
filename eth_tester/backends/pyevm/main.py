6d8d23ef3e6b5554eb203ffafc8776bc477bc227
Piper Merriam <pipermerriam@gmail.com>
2017-12-04 10:32:05 -0700

    fix py-evm backend

--- eth_tester/backends/pyevm/main.py
+++ eth_tester/backends/pyevm/main.py
@@ -224,8 +224,13 @@ class PyEVMBackend(object):
 
     def revert_to_snapshot(self, snapshot):
         block = self.chain.get_block_by_hash(snapshot)
-        self.chain = self.chain.get_chain_at_block_parent(block)
-        self.chain.import_block(block)
+        if block.number > 0:
+            self.chain.chaindb.set_as_canonical_chain_head(block.header)
+            self.chain = self.chain.get_chain_at_block_parent(block)
+            self.mine_blocks()
+        else:
+            self.chain.chaindb.set_as_canonical_chain_head(block.header)
+            self.chain = self.chain.from_genesis_header(self.chain.chaindb, block.header)
 
     def reset_to_genesis(self):
         self.account_keys, self.chain = setup_tester_chain()
