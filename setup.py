0f2611197180906f2a114bfb10b55e8a3229b3f5
Piper Merriam <pipermerriam@gmail.com>
2018-04-25 12:09:53 -0600

    Update to latest py-evm

--- setup.py
+++ setup.py
@@ -6,6 +6,44 @@ from setuptools import (
 )
 
 
+extras_require = {
+    'lint': [
+        'flake8>=3.5.0,<4.0.0',
+    ],
+    'test': [
+        'pytest>=3.2.1,<4.0.0',
+        'pytest-xdist>=1.22.2,<2',
+        'flake8==3.0.4',
+        'eth-abi>=1.0.0-beta.1,<2',
+        'eth-hash[pycryptodome]>=0.1.0a2,<1.0.0',
+    ],
+    'dev': [
+        'bumpversion>=0.5.3,<1.0.0',
+        'tox>=2.9.1,<3.0.0',
+        'wheel>=0.30.0,<1.0.0',
+    ],
+    'pyethereum16': [
+        "ethereum>=1.6.0,<2.0.0",
+        "rlp<1",
+    ],
+    'pyethereum21': [
+        "ethereum>=2.1.0,<2.2.0",
+        "rlp<1",
+    ],
+    'py-evm': [
+        # Pin py-evm to exact version, until it leaves alpha.
+        # EVM is very high velocity and might change API at each alpha.
+        "py-evm==0.2.0a16",
+    ],
+}
+
+extras_require['dev'] = (
+    extras_require['dev']
+    + extras_require['test']
+    + extras_require['lint']
+)
+
+
 setup(
     name='eth-tester',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
@@ -17,25 +55,14 @@ setup(
     url='https://github.com/ethereum/eth-tester',
     include_package_data=True,
     install_requires=[
-        "cytoolz>=0.9.0,<1.0.0",
+        "toolz>0.8.2,<1;implementation_name=='pypy'",
+        "cytoolz>=0.8.2,<1.0.0;implementation_name=='cpython'",
         "eth-utils>=1.0.1,<2.0.0",
-        "rlp>=0.6.0,<1.0.0",
+        "rlp>=0.6.0,<2.0.0",
         "semantic_version>=2.6.0,<3.0.0",
-        "eth-keys>=0.2.0-beta.2,<0.3.0",
+        "eth-keys>=0.2.0-beta.3,<0.3.0",
     ],
-    extras_require={
-        'pyethereum16': [
-            "ethereum>=1.6.0,<2.0.0",
-        ],
-        'pyethereum21': [
-            "ethereum>=2.1.0,<2.2.0",
-        ],
-        'py-evm': [
-            # Pin py-evm to exact version, until it leaves alpha.
-            # EVM is very high velocity and might change API at each alpha.
-            "py-evm==0.2.0a14",
-        ],
-    },
+    extras_require=extras_require,
     setup_requires=['setuptools-markdown'],
     py_modules=['eth_tester'],
     license="MIT",
