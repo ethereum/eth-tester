d95aefb4daf71ad00ccae896763a53ca5d058f81
NIC619 <twedusuck@gmail.com>
2018-06-13 16:12:26 +0800

    Use default accounts of each backend instead

--- eth_tester/utils/backend_testing.py
+++ eth_tester/utils/backend_testing.py
@@ -1211,25 +1211,18 @@ class BaseTestBackendDirect(object):
     #
     # Serializer
     #
-    @pytest.mark.parametrize(
-        'txs_info',
-        (
-            (
-                [
-                    {'from': '0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf', 'to': '0x2B5AD5c4795c026514f8317c7a215E218DcCD6cF', 'gas': 30000, 'value': 1},  # noqa: E501
-                    {'from': '0x2B5AD5c4795c026514f8317c7a215E218DcCD6cF', 'to': '0x6813Eb9362372EEF6200f3b1dbC3f819671cBA69', 'gas': 50000, 'value': 1},  # noqa: E501
-                    {'from': '0x6813Eb9362372EEF6200f3b1dbC3f819671cBA69', 'to': '0x1efF47bc3a10a45D4B230B5d10E37751FE6AA718', 'gas': 70000, 'value': 1},  # noqa: E501
-                    {'from': '0x1efF47bc3a10a45D4B230B5d10E37751FE6AA718', 'to': '0xe1AB8145F7E55DC933d51a18c793F901A3A0b276', 'gas': 90000, 'value': 1},  # noqa: E501
-                ]
-            ),
-        ),
-    )
-    def test_receipt_gas_used_computation(self, eth_tester, txs_info):
+    def test_receipt_gas_used_computation(self, eth_tester):
         eth_tester.disable_auto_mine_transactions()
 
         tx_hashes = []
-        for tx_info in txs_info:
-            tx_hash = eth_tester.send_transaction(tx_info)
+        for i in range(4):
+            tx = {
+                'from': eth_tester.get_accounts()[i],
+                'to': eth_tester.get_accounts()[i + 1],
+                'gas': (i + 1) * 20000 + 10000,
+                'value': 1
+            }
+            tx_hash = eth_tester.send_transaction(tx)
             tx_hashes.append(tx_hash)
         eth_tester.mine_block()
 
