ab38dfd1ce6381ca16bd4fe1e9255c9826196070
Jacques Wagener <jacques@dilectum.co.za>
2018-07-19 16:45:35 +0200

    Add reverting contract with reason message to throw contract utils.

--- eth_tester/utils/throws_contract.py
+++ eth_tester/utils/throws_contract.py
@@ -18,6 +18,17 @@ THROWS_SOURCE = (
             require(false);
         }
     }
+    """,
+    """
+    pragma solidity ^0.4.22;
+    contract DoRevert {
+        function do_revert(bool _revert) public pure returns (string message) {
+            if (_revert) {
+                revert("ribbert, ribbert");
+            }
+            return "No ribbert";
+        }
+    }
     """
 )
 
@@ -29,46 +40,67 @@ THROWS_BYTECODE = (
     "080fd5b606060a8565b005b3415606c57600080fd5b607260b6565b60405180828152602001915050"
     "60405180910390f35b3415609257600080fd5b60a6600480803590602001909190505060bc565b005"
     "b6000151560b457600080fd5b565b60005481565b80600081905550505600a165627a7a72305820d5"
-    "8a6b595eeb1a765e71924eec6e96c98c1fe9e90876ec232593b9ebb9c686500029"
+    "8a6b595eeb1a765e71924eec6e96c98c1fe9e90876ec232593b9ebb9c686500029",
+    "608060405234801561001057600080fd5b506101cd806100206000396000f30060806040526004361"
+    "0610041576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168063dfac107114610046575b600080fd5b34801561005257600080fd5b506100736004803603810190808035151590602001909291905050506100ee565b6040518080602001828103825283818151815260200191508051906020019080838360005b838110156100b3578082015181840152602081019050610098565b50505050905090810190601f1680156100e05780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b60608115610164576040517f08c379a00000000000000000000000000000000000000000000000000000000081526004018080602001828103825260108152602001807f726962626572742c20726962626572740000000000000000000000000000000081525060200191505060405180910390fd5b6040805190810160405280600a81526020017f4e6f20726962626572740000000000000000000000000000000000000000000081525090509190505600a165627a7a723058206fc7f85b6a52373bc74978757e75e3a100b44024e61485612d0f3cd27e32cdd60029"
 )
 
 
-THROWS_ABI = {
-    'willThrow': {
-        'constant': False,
-        'inputs': [],
-        'name': 'willThrow',
-        'outputs': [],
-        'payable': False,
-        'stateMutability': 'nonpayable',
-        'type': 'function',
+THROWS_ABI = (
+    {
+        'willThrow': {
+            'constant': False,
+            'inputs': [],
+            'name': 'willThrow',
+            'outputs': [],
+            'payable': False,
+            'stateMutability': 'nonpayable',
+            'type': 'function',
+        },
+        'value': {
+            'constant': True,
+            'inputs': [],
+            'name': 'value',
+            'outputs': [{'name': '', 'type': 'uint256'}],
+            'payable': False,
+            'stateMutability': 'view',
+            'type': 'function',
+        },
+        'setValue': {
+            'constant': False,
+            'inputs': [{'name': '_value', 'type': 'uint256'}],
+            'name': 'setValue',
+            'outputs': [],
+            'payable': False,
+            'stateMutability': 'nonpayable',
+            'type': 'function',
+        }
     },
-    'value': {
-        'constant': True,
-        'inputs': [],
-        'name': 'value',
-        'outputs': [{'name': '', 'type': 'uint256'}],
-        'payable': False,
-        'stateMutability': 'view',
-        'type': 'function',
-    },
-    'setValue': {
-        'constant': False,
-        'inputs': [{'name': '_value', 'type': 'uint256'}],
-        'name': 'setValue',
-        'outputs': [],
-        'payable': False,
-        'stateMutability': 'nonpayable',
-        'type': 'function',
-    },
-}
+    {
+        'do_revert': {
+            "constant": True,
+            "inputs": [{
+                "name": "_revert",
+                "type": "bool"
+            }],
+            "name": "do_revert",
+            "outputs": [{
+                "name": "message",
+                "type": "string"
+            }],
+            "payable": False,
+            "stateMutability": "pure",
+            "type": "function"
+        }
+    }
+)
 
 
-def _deploy_throws(eth_tester):
+def _deploy_throws(eth_tester, contract_idx=0):
     deploy_hash = eth_tester.send_transaction({
         "from": eth_tester.get_accounts()[0],
         "gas": 500000,
-        "data": THROWS_BYTECODE,
+        "data": THROWS_BYTECODE[contract_idx],
     })
     deploy_receipt = eth_tester.get_transaction_receipt(deploy_hash)
     throws_address = deploy_receipt['contract_address']
@@ -78,13 +110,13 @@ def _deploy_throws(eth_tester):
     return throws_address
 
 
-def _make_call_throws_transaction(eth_tester, contract_address, fn_name, fn_args=None):
+def _make_call_throws_transaction(eth_tester, contract_address, fn_name, fn_args=None, contract_idx=0):
     from eth_abi import encode_abi
 
     if fn_args is None:
         fn_args = tuple()
 
-    fn_abi = THROWS_ABI[fn_name]
+    fn_abi = THROWS_ABI[contract_idx][fn_name]
     arg_types = [
         arg_abi['type']
         for arg_abi
@@ -100,10 +132,10 @@ def _make_call_throws_transaction(eth_tester, contract_address, fn_name, fn_args
     return transaction
 
 
-def _decode_throws_result(fn_name, result):
+def _decode_throws_result(fn_name, result, contract_idx=0):
     from eth_abi import decode_abi
 
-    fn_abi = THROWS_ABI[fn_name]
+    fn_abi = THROWS_ABI[contract_idx][fn_name]
     output_types = [
         output_abi['type']
         for output_abi
