41c6fd155db0f9cb1e79fdfef5b83c10f42d6248
Jason Carver <ut96caarrs@snkmail.com>
2018-07-24 13:08:33 -0700

    Bump version: 0.1.0-beta.28 → 0.1.0-beta.29

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ extras_require['pyevm'] = extras_require['py-evm']
 setup(
     name='eth-tester',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='0.1.0-beta.28',
+    version='0.1.0-beta.29',
     description="""Tools for testing Ethereum applications.""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
