431462920dd6574402de36c1d6a9f017329f285b
Kieran Prasch <kieranprasch@gmail.com>
2018-08-19 00:31:52 -0700

    Add tests for overridable genesis parameters.

--- tests/backends/test_pyevm.py
+++ tests/backends/test_pyevm.py
@@ -14,7 +14,6 @@ from eth_tester.utils.backend_testing import (
     BaseTestBackendDirect,
 )
 
-
 @pytest.fixture
 def eth_tester():
     if not is_pyevm_available():
@@ -24,4 +23,31 @@ def eth_tester():
 
 
 class TestPyEVMBackendDirect(BaseTestBackendDirect):
-    pass
+
+    def test_reset_to_genesis(self, eth_tester):
+        super().test_reset_to_genesis(eth_tester)
+
+        param_overrides = {'gas_limit': 4750000}
+
+        # Use the the static method to generate custom genesis params
+        genesis_params = PyEVMBackend.generate_genesis_params(param_overrides)
+        assert genesis_params['block_number'] == 0
+        assert genesis_params['gas_limit'] == 4750000
+
+        # Only default genesis paraeter keys can be overriden
+        invalid_overrides = {'gato': 'con botas'}
+        with pytest.raises(ValueError):
+            _invalid_genesis_params = PyEVMBackend.generate_genesis_params(invalid_overrides)
+
+        # Initialize pyevm backend with genesis params
+        pyevm_backend = PyEVMBackend(genesis_params)
+        assert pyevm_backend.chain.header._gas_limit == 4745362
+
+        # Or use the alternate constructor
+        pyevm_backend = PyEVMBackend.from_genesis_overrides(parameter_overrides=param_overrides)
+        assert pyevm_backend.chain.header._gas_limit == 4745362
+
+        # Integrate with eth-tester
+        tester = EthereumTester(backend=pyevm_backend)
+        assert tester.backend.chain.header._block_number == 1
+        assert tester.backend.chain.header._gas_limit == 4745362
