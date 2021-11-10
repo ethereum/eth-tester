4d7aac864a43973ec33879d0bbaa770f311ea22a
kclowes <kclowes@users.noreply.github.com>
2021-11-10 14:11:02 -0700

    Bump version: 0.6.0-beta.1 → 0.6.0-beta.2

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ with open('./README.md') as readme:
 setup(
     name='eth-tester',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='0.6.0-beta.1',
+    version='0.6.0-beta.2',
     description="""Tools for testing Ethereum applications.""",
     long_description=long_description,
     long_description_content_type='text/markdown',
