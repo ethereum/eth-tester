a4a17cfdcafb658ed742f095f9467256a750e8e7
El De-dog-lo <3859395+fubuloubu@users.noreply.github.com>
2022-01-04 17:39:19 -0500

    chore: update eth-keys

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
     install_requires=[
         "eth-abi>=2.0.0b4,<3.0.0",
         "eth-account>=0.5.6,<0.6.0",
-        "eth-keys>=0.2.1,<0.4.0",
+        "eth-keys>=0.4.0,<0.5.0",
         "eth-utils>=1.4.1,<2.0.0",
         "rlp>=1.1.0,<3",
         "semantic_version>=2.6.0,<3.0.0",
