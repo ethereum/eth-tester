2c95946216cc90cd958911fb5331566f878513d6
Stuart Reed <stuart.reed@ethereum.org>
2025-04-25 17:56:16 -0600

    Implement transaction outbound normalization

--- eth_tester/normalization/common.py
+++ eth_tester/normalization/common.py
@@ -13,13 +13,37 @@ from eth_tester.utils.encoding import (
 )
 
 
-def to_lower_camel_case(key):
+def to_lower_camel_case(value):
     """
     Convert a string to lower camel case.
     """
     return "".join(
-        word.capitalize() if i else word for i, word in enumerate(key.split("_"))
+        word.capitalize() if i else word for i, word in enumerate(value.split("_"))
     )
+
+
+@curry
+@to_dict
+def normalize_dict_keys_recursive(value):
+    """
+    Normalize the keys of a dictionary using the provided normalizer.
+    """
+    for key, item in value.items():
+        if isinstance(item, dict):
+            # Recursively normalize nested dictionary keys
+            yield normalize_dict_keys_recursive(item)
+        elif isinstance(item, (list, tuple)):
+            # Recursively normalize nested list items
+            # and normalize the keys of dictionaries within the list
+            checked_items = []
+            for sub_item in item:
+                if not isinstance(sub_item, dict):
+                    checked_items.append(sub_item)
+                else:
+                    checked_items.append(normalize_dict_keys_recursive(sub_item))
+            yield to_lower_camel_case(key), tuple(checked_items)
+        else:
+            yield to_lower_camel_case(key), item
 
 
 @curry
@@ -27,7 +51,7 @@ def to_lower_camel_case(key):
 def normalize_dict(value, normalizers):
     for key, item in value.items():
         normalizer = normalizers[key]
-        yield to_lower_camel_case(key), normalizer(item)
+        yield key, normalizer(item)
 
 
 @curry
--- tests/core/normalization/test_default_outbound_normalization.py
+++ tests/core/normalization/test_default_outbound_normalization.py
@@ -1,7 +1,16 @@
 import pytest
+from eth_utils import (
+    to_checksum_address,
+)
 
 from eth_tester.normalization import (
     DefaultNormalizer,
+)
+from tests.core.validation.test_inbound_validation import (
+    _make_transaction,
+)
+from tests.core.validation.test_outbound_validation import (
+    _make_dynamic_fee_txn,
 )
 from tests.utils import (
     make_receipt,
@@ -41,3 +50,109 @@ def test_outbound_receipt_contract_address_status_based_normalization(
 )
 def test_outbound_storage_normalization(value, expected):
     assert DefaultNormalizer.normalize_outbound_storage(value) == expected
+
+
+def test_outbound_transaction_normalization() -> None:
+    transaction = {
+        "type": 2,
+        "blob_versioned_hashes": (
+            b"\x00" * 32,
+            b"\x00" * 32,
+            b"\x00" * 32,
+            b"\x00" * 32,
+        ),
+        "chain_id": 131277322940537,
+        "hash": b"\x00" * 32,
+        "nonce": 0,
+        "block_hash": b"\x00" * 32,
+        "block_number": 0,
+        "transaction_index": 0,
+        "from": b"\x00" * 20,
+        "to": b"\x00" * 20,
+        "value": 0,
+        "gas": 21000,
+        "gas_price": 1,
+        "max_fee_per_blob_gas": 1,
+        "max_fee_per_gas": 2000000000,
+        "max_priority_fee_per_gas": 1000000000,
+        "data": b"",
+        "access_list": (
+            (
+                b"\x00" * 20,
+                (
+                    0x01,
+                    0x02,
+                    0x03,
+                    0x04,
+                ),
+            ),
+        ),
+        "authorization_list": (
+            {
+                "chain_id": 1,
+                "address": b"\x00" * 20,
+                "nonce": 0,
+                "y_parity": 0,
+                "r": 0,
+                "s": 0,
+            },
+        ),
+        "r": 0,
+        "s": 0,
+        "v": 0,
+        "y_parity": 0,
+    }
+    normalized_transaction = DefaultNormalizer.normalize_outbound_transaction(
+        transaction
+    )
+
+    expected_normalized_transaction = {
+        "type": "0x2",
+        "blobVersionedHashes": (
+            "0x" + "00" * 32,
+            "0x" + "00" * 32,
+            "0x" + "00" * 32,
+            "0x" + "00" * 32,
+        ),
+        "chainId": 131277322940537,
+        "hash": "0x" + "00" * 32,
+        "nonce": 0,
+        "blockHash": "0x" + "00" * 32,
+        "blockNumber": 0,
+        "transactionIndex": 0,
+        "from": "0x0000000000000000000000000000000000000000",
+        "to": "0x0000000000000000000000000000000000000000",
+        "value": 0,
+        "gas": 21000,
+        "gasPrice": 1,
+        "maxFeePerBlobGas": 1,
+        "maxFeePerGas": 2000000000,
+        "maxPriorityFeePerGas": 1000000000,
+        "data": "0x",
+        "accessList": (
+            {
+                "address": "0x0000000000000000000000000000000000000000",
+                "storageKeys": (
+                    "0x" + "00" * 31 + "01",
+                    "0x" + "00" * 31 + "02",
+                    "0x" + "00" * 31 + "03",
+                    "0x" + "00" * 31 + "04",
+                ),
+            },
+        ),
+        "authorizationList": (
+            {
+                "chainId": 1,
+                "address": "0x0000000000000000000000000000000000000000",
+                "nonce": 0,
+                "yParity": 0,
+                "r": 0,
+                "s": 0,
+            },
+        ),
+        "r": 0,
+        "s": 0,
+        "v": 0,
+        "yParity": 0,
+    }
+    assert normalized_transaction == expected_normalized_transaction
